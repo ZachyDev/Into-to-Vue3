d9858ff9359acdb63df3a5653df8310879f94764
ZachyDev <mosetizachary001@gmail.com>
2020-10-09 00:42:41 +0300

    Conditional Rendering

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            productDescription: "Men's Socks"
+            image: './assets/images/socks_green.jpg',
+            inventory: 0
         }
     }
 })
